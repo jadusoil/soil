035bfa8e7070a849553089da256b02dd04138123
yadavparmatma <pyadav.it@gmail.com>
2020-07-04 13:10:38 +0530

    Add deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         publicPath: '/',
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         filename: 'bundle.js'
     },
     module: {
